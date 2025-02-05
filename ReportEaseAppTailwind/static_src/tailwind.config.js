64b0d09623f2ae5a7e7a80be6d25d1487e0df96b
Nirbhik Dhakal <nirbhikdhakal8@gmail.com>
2025-02-05 21:32:28 +0545

    citizen user frontend 95% finished

--- ReportEaseAppTailwind/static_src/tailwind.config.js
+++ ReportEaseAppTailwind/static_src/tailwind.config.js
@@ -42,18 +42,71 @@ module.exports = {
         // '../../**/*.py'
     ],
     theme: {
-        colors:{
-            "dark-blue":"#2C3E50",
-            "white":"#ffffff",
-            "black":"#000000",
-            "cool-gray":"#95A5A6",
-            "light-gray":"#f2f0ef",
-            "gray":"#d3d3d3"
+        colors: {
+            "dark-blue": "#2C3E50",
+            "white": "#ffffff",
+            "black": "#000000",
+            "cool-gray": "#95A5A6",
+            "light-gray": "#f2f0ef",
+            "gray": "#d3d3d3",
+            "dark-yellow": "#edaf28",
         },
         extend: {
             backgroundImage: {
                 'Homepage-bcg': "url('/static/ReportEaseApp/Homepage/image.png')",
-              },
+
+            },
+
+            animation: {
+                blink: 'blink 1.4s infinite both',
+                fade: 'fade 1.4s infinite both',
+                scale: 'scale 1.5s infinite',
+                perspective: 'perspective 1.2s infinite',
+                fadeIn: 'fadeIn 1.2s ease-in-out infinite both',
+            },
+            keyframes: {
+                blink: {
+                    '0%': {
+                        opacity: '0.2',
+                    },
+                    '20%': {
+                        opacity: '1',
+                    },
+                    '100%': {
+                        opacity: ' 0.2',
+                    },
+                },
+                fade: {
+                    '0%, 100%': {
+                        opacity: '1',
+                    },
+                    '50%': {
+                        opacity: ' 0.3',
+                    },
+                },
+                fadeIn: {
+                    '0%, 39%, 100%': {
+                        opacity: '0',
+                    },
+                    '40%': {
+                        opacity: '1',
+                    },
+                },
+                scale: {
+                    '0%, 100%': {
+                        transform: 'scale(1.0)',
+                    },
+                    '50%': {
+                        transform: 'scale(0.7)',
+                        // change this to set how small you want the element to shrink
+                    },
+                },
+                perspective: {
+                    '0%': { transform: 'perspective(120px)' },
+                    ' 50%': { transform: 'perspective(120px) rotateY(180deg)' },
+                    '100%': { transform: 'perspective(120px) rotateY(180deg)  rotateX(180deg)' },
+                },
+            },
         },
     },
     plugins: [
@@ -65,5 +118,21 @@ module.exports = {
         require('@tailwindcss/forms'),
         require('@tailwindcss/typography'),
         require('@tailwindcss/aspect-ratio'),
+
+        // !!!! This plugin should be copied for all loading spinners !!!!
+        // plugin (({ matchUtilities, theme }) => {
+        //     matchUtilities(
+        //         {
+        //             'animation-delay': (value) => {
+        //                 return {
+        //                     'animation-delay': value,
+        //                 }
+        //             },
+        //         },
+        //         {
+        //             values: theme('transitionDelay'),
+        //         }
+        //     )
+        // }),
     ],
 }
