3ce5daea832120a8d154dd6d626ef9ab7a422838
Nirbhik Dhakal <nirbhikdhakal8@gmail.com>
2025-04-25 11:03:21 +0545

    searching and filtering of cases

--- Citizen/urls.py
+++ Citizen/urls.py
@@ -2,8 +2,10 @@ from django.conf import settings
 from django.contrib import admin
 from django.urls import path,include
 from django.conf.urls.static import static
+from .views import register_case
 
 urlpatterns = [
+    path('registercase/', register_case, name='register_case'),
 ]
 
 if settings.DEBUG:  # Serve media files only in development
--- ReportEase/settings.py
+++ ReportEase/settings.py
@@ -69,6 +69,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    # 'django.middleware.cache.UpdateCacheMiddleware',
+    # 'django.middleware.cache.FetchFromCacheMiddleware',
+    # 'userauths.middleware.IgnoreClientDisconnect',
     # 'userauths.middleware.RedirectWithJSMessageMiddleware',
 ]
 
