5066046cce034921591defe8f98e9118614213e6
Nirbhik Dhakal <nirbhikdhakal8@gmail.com>
2025-05-23 20:56:43 +0545

    is_read set to true when both users online

--- chat/consumers.py
+++ chat/consumers.py
@@ -4,47 +4,104 @@ from channels.db import database_sync_to_async
 from Case.models import Case
 from .models import Room,Message
 from userauths.models import Citizen as Ct, Investigator as Iv
+import redis
+from asgiref.sync import sync_to_async
+
+r = redis.StrictRedis(host='localhost', port=6379, db=0)
+
+@sync_to_async
+def get_user_from_session(session):
+    return {
+        "user_type": session.get("user_type"),
+        "user_id": session.get("user_id")
+    }
 
 class ChatConsumer(AsyncWebsocketConsumer):
+    
+    @sync_to_async
+    def set_user_online(self):
+        r.set(self.user_key, "online")
+
+    @sync_to_async
+    def set_user_offline(self):
+        r.delete(self.user_key)
+
+    @sync_to_async
+    def is_user_online(self,user_key):
+        return r.exists(user_key) == 1
+    
+    @sync_to_async
+    def get_other_user_key(self):
+        current_case = Case.objects.get(case_id=self.case_id)
+        if self.user_type == "Citizen":
+            return f"Investigator_{current_case.investigator.user_id}"
+        elif self.user_type == "Investigator":
+            return f"Citizen_{current_case.reporter.user_id}"
+        return None
 
     async def connect(self):
         self.room_name = self.scope['url_route']['kwargs']['case_id']
+        self.case_id = self.scope['url_route']['kwargs']['case_id']
+        
+        user_data = await get_user_from_session(self.scope["session"])
+        self.user_type = user_data["user_type"]
+        self.user_id = user_data["user_id"]
+        self.user_key = f"{self.user_type}_{self.user_id}"
+        
+        await self.set_user_online()
+        
+        await self.channel_layer.group_add(self.user_key, self.channel_name)
         await self.channel_layer.group_add(self.room_name, self.channel_name)
 
         await self.accept()
 
     async def disconnect(self, code):
+        await self.set_user_offline()
+        await self.channel_layer.group_discard(self.user_key, self.channel_name)
         await self.channel_layer.group_discard(self.room_name, self.channel_name)
         self.close(code)
         
     async def receive(self,text_data):
         data_json = json.loads(text_data)
-        print('receive ma cha---',data_json)
         event = {
             'type': 'send_message',
             'message': data_json,
         }
         
         await self.channel_layer.group_send(self.room_name, event)
+        await self.create_message(data=data_json)
         
     async def send_message(self, event):
         data = event["message"]
-        # print('send ma cha---',data)
-        # await self.create_message(data=data)
         response = {
-                "sender":data["sender"],
-                "message":data["message"],
-            }
-        await self.send(text_data=json.dumps({"message":response})) 
+            "sender": data["sender"],
+            "message": data["message"],
+        }
+        await self.send(text_data=json.dumps({"message": response}))
+        
+        # Check if other user is online and mark message as read
+        other_user_key = await self.get_other_user_key()
+        if other_user_key and await self.is_user_online(other_user_key):
+            await self.mark_message_as_read(data)
+        
+    @database_sync_to_async
+    def get_other_user(self):
+        current_case = Case.objects.get(case_id=self.case_id)
+        if self.user_type == "Citizen":
+            return current_case.investigator.user_id
+        elif self.user_type == "Investigator":
+            return current_case.reporter.user_id
+        return None
         
     @database_sync_to_async
     def create_message(self, data):
-        get_case=Case.objects.get(case_id=data["room_name"])
+        get_case = Case.objects.get(case_id=data["room_name"])
         session = self.scope["session"]
         user_id = session.get("user_id")
         user_type = session.get("user_type") 
         sender_citizen = None
         sender_investigator = None
+        
         if user_id and user_type == "Citizen":
             sender_citizen = Ct.objects.get(user_id=user_id)
         elif user_id and user_type == "Investigator":
@@ -55,10 +112,17 @@ class ChatConsumer(AsyncWebsocketConsumer):
                 case=get_case,
                 sender_citizen=sender_citizen,
                 sender_investigator=sender_investigator,
-                content=data["message"]
+                content=data["message"],
+                is_read=False  # Explicitly set is_read to False for new messages
             )
             new_message.save()
                 
-            
-            
+    @database_sync_to_async
+    def mark_message_as_read(self, data):
+        get_case = Case.objects.get(case_id=data["room_name"])
+        Message.objects.filter(
+            case=get_case,
+            is_read=False
+        ).update(is_read=True)
+        
         
